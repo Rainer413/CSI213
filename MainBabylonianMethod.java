0b930210ddfe6de236341fcf2015c40809971e1c
Rainer Alvarado <ralvarado@albany.edu>
2016-10-02 22:46:13 -0400

    Main Babylonian method

--- MainBabylonianMethod.java
+++ MainBabylonianMethod.java
@@ -15,15 +15,15 @@ public class MainBabylonianMethod
     //welcome user to the program and prompt user for preferences
     System.out.print("Enter the number that you would like to take the square root of?");
     num = scan.nextDouble();
-    System.out.print("\nHow much error will you tolerate in the estimation?");
+    System.out.print("How much error will you tolerate in the estimation?");
     error = scan.nextDouble();
     
     //make an instance of the BabylonianSqrt class
-    BabylonianSqrt root = new BabylonianSqrt(num, error);
+    Recursion root = new Recursion(num, error);
     
     //print the babylonian square root according to the user's preferences
-    System.out.println("\n\nThe approximate square root of " + num + " is "
-                      + root.babylonianRoot());
+    System.out.println("The approximate square root of " + num + " is "
+                      + root.BabylonianRoots());
   }
   
 }
