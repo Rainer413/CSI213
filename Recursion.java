41f35884a35f719d8d89ff435f2916670173ec20
Rainer Alvarado <ralvarado@albany.edu>
2016-10-02 22:39:04 -0400

    If else statement

--- Recursion.java
+++ Recursion.java
@@ -2,15 +2,38 @@
 Rainer Alvarado
 CSI213
 */
-public class Babylonian_Recursion 
+public class Recursion 
 {
   private double guess; 
   private double num, error;
   
-  public BabylonianSqrt() 
+  public BabylonianMethod() 
   {
     num = 0.0;
     error = 0.0;
     guess = 0.0; 
   }
   
+  public BabylonianMethod(double x, double y) 
+  {
+    num = x; 
+    error = y;
+    guess = num / 2.0;
+  }
+
+ public void setGuess(double lastGuess)
+ {
+   guess = .5*(lastGuess + (num / lastGuess)); 
+ }
+ 
+ public double BabylonianRoots()
+ {
+   if (Math.abs(num - (guess * guess)) <= error) 
+     return guess;
+    else
+    {
+      setGuess(guess);
+      return babylonianRoot();
+    }
+  }
+}
